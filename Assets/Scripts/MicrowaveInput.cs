f8313a997c871bef08fe65de340562100eb97549
Rebecca <rebeccamitchell88@gmail.com>
2017-01-21 01:43:46 -0500

    Initial Push

---
+++ Assets/Scripts/MicrowaveInput.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Rigidbody))]
+public class MicrowaveInput : MonoBehaviour {
+    
+    [SerializeField] int playerID;
+    Player player;
+
+    Pickup heldPickup;
+
+	// Use this for initialization
+	void Start () {
+        player = ReInput.players.GetPlayer(playerID);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 moveDirection = Vector3.zero;
+
+        // Left/Right
+        if (player.GetAxisRaw("Horizontal") > 0) {
+            
+            moveDirection += new Vector3(0.1f, 0, 0);
+        }
+        else if (player.GetAxisRaw("Horizontal") < 0) {
+            
+            moveDirection += new Vector3(-0.1f, 0, 0);
+        }
+
+        // Foreward/Backward
+        if (player.GetAxisRaw("Vertical") > 0) {
+            
+            moveDirection += new Vector3(0, 0, 0.1f);
+        }
+        else if (player.GetAxisRaw("Vertical") < 0) {
+            
+            moveDirection += new Vector3(0, 0, -0.1f);
+        }
+
+        if (moveDirection != Vector3.zero) {
+
+            this.GetComponent<Rigidbody>().MovePosition(this.transform.position + moveDirection);
+        }
+        RotateMicrowave();
+	}
+
+    void RotateMicrowave()  {
+        float horz = player.GetAxisRaw("Horizontal"); //grabs the float value for the joystick on its x axis
+        float vert = player.GetAxisRaw("Vertical"); //grabs the float value for the joystick on its x axis
+        float tarAngle = Mathf.Atan2(vert, horz) * Mathf.Rad2Deg; //converting the joystick vectors into a rotational angle around the z axis
+
+        if (player.GetAxisRaw("Vertical") != 0 || player.GetAxisRaw("Horizontal") != 0) {
+            
+            this.transform.rotation = Quaternion.Euler(0, 270 - tarAngle, 0); //changes the z rotation of the pointer to the z rotation of the joystick. (-90) to sync unity rotaion values and controller values
+        }
+    }
+
+    void ThrowObject() {
+        // Collect Forces the longer the A button is being held
+    }
+
+    public void PickupObject(Pickup newObject) {
+        heldPickup = newObject;
+    }
+}
---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+    public enum PickupType { FOOD, ROCK }
+    [SerializeField] PickupType type;
+    [SerializeField] float cookTime;
+
+    bool projectile = false;
+
+    void OnCollisionEnter(Collision other) {
+        if (projectile && other.gameObject.CompareTag("Player")) {
+            // Attack Player
+
+        } else if (other.gameObject.CompareTag("Player")) {
+            // Enter Player
+            Destroy(gameObject);
+        }
+    }
+}
